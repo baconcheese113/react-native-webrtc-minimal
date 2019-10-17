d212a8a2b352f86ac1af530329816198024b95b3
ERROR\Name <baconcheese113@hotmail.com>
2019-10-16 22:36:46 -0600

    Gutted dependencies and polished core functionality

--- src/App.js
+++ src/App.js
@@ -4,17 +4,9 @@
  */
 
 import React from 'react';
-import {Text, View, Button, StyleSheet} from 'react-native';
+import {View, Button, StyleSheet} from 'react-native';
 
-import {
-  RTCPeerConnection,
-  RTCIceCandidate,
-  RTCSessionDescription,
-  RTCView,
-  MediaStream,
-  MediaStreamTrack,
-  mediaDevices,
-} from 'react-native-webrtc';
+import {RTCPeerConnection, RTCView, mediaDevices} from 'react-native-webrtc';
 
 export default function App() {
   const [localStream, setLocalStream] = React.useState();
@@ -22,14 +14,12 @@ export default function App() {
   const [cachedLocalPC, setCachedLocalPC] = React.useState();
   const [cachedRemotePC, setCachedRemotePC] = React.useState();
 
-  const setLocalStreamsUp = async () => {
+  const startLocalStream = async () => {
     const isFront = true;
     const devices = await mediaDevices.enumerateDevices();
 
     const facing = isFront ? 'front' : 'back';
-    const videoSourceId = devices.find(
-      device => device.kind === 'videoinput' && device.facing === facing,
-    );
+    const videoSourceId = devices.find(device => device.kind === 'videoinput' && device.facing === facing);
     const facingMode = isFront ? 'user' : 'environment';
     const constraints = {
       audio: true,
@@ -45,35 +35,35 @@ export default function App() {
     };
     const newStream = await mediaDevices.getUserMedia(constraints);
     setLocalStream(newStream);
+  };
 
+  const startCall = async () => {
     const configuration = {iceServers: [{url: 'stun:stun.l.google.com:19302'}]};
     const localPC = new RTCPeerConnection(configuration);
     const remotePC = new RTCPeerConnection(configuration);
-    localPC.addEventListener('icecandidate', e => {
+
+    // could also use "addEventListener" for these callbacks, but you'd need to handle removing them as well
+    localPC.onicecandidate = e => {
       try {
-        console.log(`localPC icecandidate:`);
-        console.log(e.candidate);
-        console.log(remotePC);
+        console.log('localPC icecandidate:', e.candidate);
         if (e.candidate) {
           remotePC.addIceCandidate(e.candidate);
         }
       } catch (err) {
         console.error(`Error adding remotePC iceCandidate: ${err}`);
       }
-    });
-    remotePC.addEventListener('icecandidate', e => {
+    };
+    remotePC.onicecandidate = e => {
       try {
-        console.log(`remotePC icecandidate:`);
-        console.log(e.candidate);
-        console.log(localPC);
+        console.log('remotePC icecandidate:', e.candidate);
         if (e.candidate) {
           localPC.addIceCandidate(e.candidate);
         }
       } catch (err) {
         console.error(`Error adding localPC iceCandidate: ${err}`);
       }
-    });
-    remotePC.addEventListener('addstream', e => {
+    };
+    remotePC.onaddstream = e => {
       console.log('remotePC tracking with ', e);
       if (e.stream && remoteStream !== e.stream) {
         console.log('RemotePC received the stream', e.stream);
@@ -81,9 +71,9 @@ export default function App() {
       }
     };
 
-    // AddTrack not supported yet
+    // AddTrack not supported yet, so have to use old school addStream instead
     // newStream.getTracks().forEach(track => localPC.addTrack(track, newStream));
-    localPC.addStream(newStream);
+    localPC.addStream(localStream);
     try {
       const offer = await localPC.createOffer();
       console.log('Offer from localPC, setLocalDescription');
@@ -106,48 +96,54 @@ export default function App() {
 
   const closeStreams = () => {
     if (cachedLocalPC) {
+      cachedLocalPC.removeStream(localStream);
       cachedLocalPC.close();
     }
     if (cachedRemotePC) {
+      cachedRemotePC.removeStream(remoteStream);
       cachedRemotePC.close();
     }
+    setLocalStream();
+    setRemoteStream();
+    setCachedRemotePC();
+    setCachedLocalPC();
   };
 
   return (
-    <View>
-      <Text style={styles.text}>{`Stream is ${
-        localStream ? 'Defined' : 'not defined'
-      }`}</Text>
-      <Button title="Click to start stream" onPress={setLocalStreamsUp} />
+    <View style={styles.container}>
+      {!localStream && <Button title="Click to start stream" onPress={startLocalStream} />}
+      {localStream && <Button title="Click to start call" onPress={startCall} disabled={!!remoteStream} />}
+
       <View style={styles.rtcview}>
-        {localStream && (
-          <RTCView style={styles.rtc} streamURL={localStream.toURL()} />
-        )}
+        {localStream && <RTCView style={styles.rtc} streamURL={localStream.toURL()} />}
       </View>
       <View style={styles.rtcview}>
-        {remoteStream && (
-          <RTCView style={styles.rtc} streamURL={remoteStream.toURL()} />
-        )}
+        {remoteStream && <RTCView style={styles.rtc} streamURL={remoteStream.toURL()} />}
       </View>
-      <Button title="Click to stop call" onPress={closeStreams} />
+      <Button title="Click to stop call" onPress={closeStreams} disabled={!remoteStream} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#313131',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+  },
   text: {
     fontSize: 30,
   },
   rtcview: {
     justifyContent: 'center',
     alignItems: 'center',
-    height: 150,
+    height: '40%',
     width: '80%',
-    margin: 10,
     backgroundColor: 'black',
   },
   rtc: {
     width: '80%',
-    height: 150,
+    height: '100%',
   },
 });
